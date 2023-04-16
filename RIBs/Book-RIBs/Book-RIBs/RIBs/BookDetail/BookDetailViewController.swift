8a53cdee65bdfc49e8042a5608e2b0456f23517d
textobey <dogsan_e@naver.com>
2023-04-16 19:52:46 +0900

    [ADD] BookDetail UI

--- RIBs/Book-RIBs/Book-RIBs/RIBs/BookDetail/BookDetailViewController.swift
+++ RIBs/Book-RIBs/Book-RIBs/RIBs/BookDetail/BookDetailViewController.swift
@@ -22,8 +22,72 @@ final class BookDetailViewController: UIViewController, BookDetailPresentable, B
 
     weak var listener: BookDetailPresentableListener?
     
+    // MARK: UI
+    
+    lazy var boxView = UIView().then {
+        $0.backgroundColor = .systemGray5
+    }
+    
+    lazy var imageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    lazy var stackView = UIStackView().then {
+        $0.alignment = .leading
+        $0.axis = .vertical
+        $0.distribution = .equalSpacing
+        $0.spacing = 6
+    }
+    
+    lazy var titleLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 16, weight: .bold)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+        $0.text = nil
+    }
+    
+    lazy var subtitleLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+        $0.text = nil
+    }
+    
+    lazy var isbn13Label = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+        $0.text = nil
+    }
+    
+    lazy var priceLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+        $0.text = nil
+    }
+    
+    lazy var urlLinkLabel = UILabel().then {
+        $0.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        $0.textAlignment = .left
+        $0.numberOfLines = 0
+        $0.textColor = .systemBlue
+        $0.text = nil
+    }
+    
+    lazy var textView = UITextView().then {
+        $0.text = "메모를 입력해보세요"
+        $0.delegate = self
+        $0.returnKeyType = .done
+        $0.textColor = .systemGray3
+        $0.layer.borderWidth = 2
+        $0.layer.cornerRadius = 10
+        $0.layer.borderColor = UIColor.systemGray6.cgColor
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Book Detail"
         view.backgroundColor = .systemBackground
         setupLayout()
         bindAction()
@@ -39,7 +103,37 @@ final class BookDetailViewController: UIViewController, BookDetailPresentable, B
     }
     
     private func setupLayout() {
+        view.addSubview(boxView)
+        boxView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(230)
+        }
         
+        boxView.addSubview(imageView)
+        imageView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(30)
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(100)
+        }
+        
+        view.addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(boxView.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        }
+        
+        view.addSubview(textView)
+        textView.snp.makeConstraints {
+            $0.top.equalTo(stackView.snp.bottom).offset(20)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            $0.height.equalTo(230)
+        }
+        
+        [titleLabel, subtitleLabel, isbn13Label, priceLabel, urlLinkLabel].forEach {
+            stackView.addArrangedSubview($0)
+        }
     }
 }
 
@@ -54,5 +148,41 @@ extension BookDetailViewController {
             .map { $0.0.listener?.popViewController(true) }
             .subscribe()
             .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - TextViewDelegate
+
+extension BookDetailViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        // FIXME: - keyboard 높이만큼 scroll view도 올라가야함 -> NotificationCenter
+        if textView.textColor != .black {
+            textView.text.removeAll()
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        guard let title = self.titleLabel.text else { return }
+        guard let text = textView.text else { return }
+        
+        if textView.textColor == .systemGray3 || text == "" {
+            self.textView.text = "메모를 입력해보세요"
+            self.textView.textColor = .systemGray3
+            if UserDefaults.standard.string(forKey: title) != nil {
+                UserDefaults.standard.removeObject(forKey: title)
+            }
+        } else {
+            UserDefaults.standard.set(text, forKey: title)
+        }
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            // MARK: - keyboard 내려갈때 scrollView도 내려가기 -> NotificationCenter
+            return false
+        }
+        return true
     }
 }
