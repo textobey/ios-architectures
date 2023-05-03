994d01d75f43861e083ed2ce7c7fed8b17caf638
textobey <dogsan_e@naver.com>
2023-05-03 17:21:39 +0900

    [ADD] Book-SwiftUI

---
+++ SwiftUI/Book-SwiftUI/Book-SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Book-SwiftUI
+//
+//  Created by 이서준 on 2023/05/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
