296193880b953bf74878b1e15bbe4c5fd5b2a69d
textobey <dogsan_e@naver.com>
2023-05-16 18:09:34 +0900

    [ADD] Binding

---
+++ SwiftUI-MV/Book-SwiftUI/Book-SwiftUI/View/NewBook/NewBookListRow.swift
@@ -0,0 +1,138 @@
+//
+//  NewBookListRow.swift
+//  Book-SwiftUI
+//
+//  Created by 이서준 on 2023/05/16.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct NewBookListRow: View {
+    
+    @State private var isBookmarked = false
+    
+    var bookItem: BookItem?
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            ZStack {
+                KFImage(URL(string: bookItem?.image ?? ""))
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 140)
+                
+                Button(action: {
+                    isBookmarked.toggle()
+                }, label: {
+                    Image(systemName: isBookmarked ? "star.fill" : "star")
+                        .resizable()
+                        .frame(width: 22, height: 20)
+                        .foregroundColor(Color.black.opacity(0.6))
+                })
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+            }
+            //.frame(maxWidth: .infinity, alignment: .center)
+            .background(Color(.systemGray6))
+            
+            
+            VStack(spacing: 5) {
+                Text(bookItem?.title ?? "")
+                    .font(.system(size: 16, weight: .bold))
+                
+                Text(bookItem?.subtitle ?? "")
+                    .font(.system(size: 14, weight: .medium))
+                
+                Text(bookItem?.id ?? "")
+                    .font(.system(size: 12, weight: .light))
+                
+                Text(bookItem?.price ?? "")
+                    .font(.system(size: 12, weight: .medium))
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .multilineTextAlignment(.center)
+            .padding(.top, 15)
+            .padding(.bottom, 15)
+            .background(Color(.systemGray5))
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .padding(.top, 10)
+        .padding(.bottom, 10)
+        
+    }
+}
+
+struct NewBookListRow_Previews: PreviewProvider {
+    static var previews: some View {
+        NewBookListRow(
+            bookItem: BookItem(
+                id: "isbn13",
+                title: "title",
+                subtitle: "subtitle",
+                price: "price",
+                image: "https://itbook.store/img/books/9781098116743.png",
+                url: ""
+            )
+        )
+    }
+}
+
+
+
+//var body: some View {
+//    VStack(spacing: 15) {
+//        ZStack {
+//            KFImage(URL(string: bookItem?.image ?? ""))
+//                .resizable()
+//                .aspectRatio(contentMode: .fit)
+//                .padding()
+//        }
+//        .frame(height: 200)
+//
+//        VStack(alignment: .center, spacing: 10) {
+//            Text(bookItem?.title ?? "")
+//                .font(.title)
+//                .fontWeight(.bold)
+//
+//            Text(bookItem?.subtitle ?? "")
+//                .font(.subheadline)
+//                .foregroundColor(.secondary)
+//
+//            HStack(spacing: 5) {
+//                Image(systemName: "barcode.viewfinder")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: 16, height: 16)
+//                Text(bookItem?.id ?? "")
+//                    .font(.caption)
+//                    .foregroundColor(.secondary)
+//            }
+//
+//            HStack(spacing: 5) {
+//                Image(systemName: "dollarsign.circle.fill")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: 16, height: 16)
+//                Text(bookItem?.price ?? "")
+//                    .font(.caption)
+//                    .foregroundColor(.secondary)
+//            }
+//
+//            Button(action: {
+//                isBookmarked.toggle()
+//            }) {
+//                Image(systemName: isBookmarked ? "star.fill" : "star")
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+//                    .frame(width: 20, height: 20)
+//                    .foregroundColor(.yellow)
+//            }
+//        }
+//        .padding()
+//        //.background(UIColor.systemGray5)
+//        .cornerRadius(10)
+//        .padding()
+//
+//        Spacer()
+//    }
+//}
--- SwiftUI-MV/Book-SwiftUI/Book-SwiftUI/View/NewBook/NewBookView.swift
+++ SwiftUI-MV/Book-SwiftUI/Book-SwiftUI/View/NewBook/NewBookView.swift
@@ -6,71 +6,46 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct NewBookView: View {
     
     // MARK: - Properties
     
-    @State private var isBookmarked = false
+    //@State private var isBookmarked = false
+    //@ObservedObject var bookModel: BookModel = BookModel()
+    @State var bookModel: BookModel?
     
     var body: some View {
         NavigationView {
-            VStack(spacing: 15) {
-                ZStack {
-                    Image(systemName: "book.fill")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding()
+            ZStack {
+                List {
+                    ForEach(bookModel?.books ?? [], id: \.id) { book in
+                        ZStack {
+                            NewBookListRow(bookItem: book)
+                        }
+                        .padding(.leading, 20)
+                        .padding(.trailing, 20)
+                        .listRowSeparator(.hidden)
+                    }
+                    .listRowInsets(EdgeInsets())
                 }
-                .frame(height: 200)
-                
-                VStack(alignment: .center, spacing: 10) {
-                    Text("Book title")
-                        .font(.title)
-                        .fontWeight(.bold)
-                    
-                    Text("Book Subtitle")
-                        .font(.subheadline)
-                        .foregroundColor(.secondary)
-                    
-                    HStack(spacing: 5) {
-                        Image(systemName: "barcode.viewfinder")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 16, height: 16)
-                        Text("ISBN13: 1234567890123")
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                    
-                    HStack(spacing: 5) {
-                        Image(systemName: "dollarsign.circle.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 16, height: 16)
-                        Text("$ 9.99")
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                    
-                    Button(action: {
-                        isBookmarked.toggle()
-                    }) {
-                        Image(systemName: isBookmarked ? "star.fill" : "star")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 20, height: 20)
-                            .foregroundColor(.yellow)
-                    }
-                }
-                .padding()
-                //.background(UIColor.systemGray5)
-                .cornerRadius(10)
-                .padding()
-                
-                Spacer()
+                .listStyle(.plain)
             }
-            .navigationTitle("New Book")
+            .navigationBarTitle("New Book", displayMode: .large)
+        }
+        .onAppear {
+            NetworkService.shared.request(.new, completion: { result in
+                switch result {
+                case .success(let bookModel):
+                    DispatchQueue.main.async {
+                        print(bookModel)
+                        self.bookModel = bookModel
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            })
         }
     }
 }
