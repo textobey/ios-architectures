63c6d9ffa7951eba61ac54c48958af2bcd06bf75
textobey <dogsan_e@naver.com>
2023-05-26 18:09:25 +0900

    [ADD] SearchBook Layout

--- SwiftUI-MV/Book-SwiftUI/Book-SwiftUI/View/SearchBook/SearchBookView.swift
+++ SwiftUI-MV/Book-SwiftUI/Book-SwiftUI/View/SearchBook/SearchBookView.swift
@@ -8,8 +8,39 @@
 import SwiftUI
 
 struct SearchBookView: View {
+    
+    @State var bookModel: BookModel?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                ForEach(bookModel?.books ?? [], id: \.id) { book in
+                    ZStack {
+                        SearchBookListRow(bookItem: book)
+                    }
+                    .padding(.leading, 20)
+                    .padding(.trailing, 20)
+                    .listRowSeparator(.hidden)
+                }
+                .listRowInsets(EdgeInsets())
+                .listRowBackground(Color.clear)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Search Books")
+        }
+        .onAppear {
+            NetworkService.shared.request(.search(word: "Apple", page: 1), completion: { result in
+                switch result {
+                case .success(let bookModel):
+                    DispatchQueue.main.async {
+                        print(bookModel)
+                        self.bookModel = bookModel
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            })
+        }
     }
 }
 
