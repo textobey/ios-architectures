d273fff7ec9c6c5ff94492051b60024581270c2c
textobey <dogsan_e@naver.com>
2023-12-21 18:46:57 +0900

    [FIX] UI 백그라운드 스레드 업데이트 이슈 수정

--- SwiftUI-MVVM/SwiftUI-MVVM/Service/URLImageLoader.swift
+++ SwiftUI-MVVM/SwiftUI-MVVM/Service/URLImageLoader.swift
@@ -12,6 +12,7 @@ import SwiftUI
  그 미만 버전에서는 URLSession을 통해 데이터를 받아와 이미지로 변환해야해요.
  */
 
+@MainActor
 class URLImageLoader: ObservableObject {
     @Published var image: UIImage?
     
@@ -23,24 +24,42 @@ class URLImageLoader: ObservableObject {
     
     private func loadImage(urlString: String?) {
         guard let urlString = urlString else { return }
+        
+        if let imageFromCache = getImageFromCache(from: urlString) {
+            self.image = imageFromCache
+            return
+        }
+        loadImageFromURL(urlString: urlString)
+    }
+    
+    /*
+     MainActor 어트리뷰트를 지정하여 Task 블록의 작업을 메인스레드에서 수행하도록 자동으로 라우팅됨
+     */
+    
+    private func loadImageFromURL(urlString: String) {
+        guard let url = URL(string: urlString) else { return }
+        
         Task {
-            await loadImageFromURL(urlString: urlString)
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                
+                guard let loadedImage = UIImage(data: data) else {
+                    print("❎ Failed Parse Data to Image ")
+                    return
+                }
+                self.image = loadedImage
+                self.setImageCache(image: loadedImage, key: urlString)
+            } catch {
+                print("❎ Failed \(#function) Error: \(error)")
+            }
         }
     }
     
-    private func loadImageFromURL(urlString: String) async {
-        guard let url = URL(string: urlString) else { return }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            guard let loadedImage = UIImage(data: data) else {
-                print("❎ Failed Parse Data to Image ")
-                return
-            }
-            self.image = loadedImage
-        } catch {
-            print("❎ Failed \(#function) Error: \(error)")
-        }
+    private func setImageCache(image: UIImage, key: String) {
+        imageCache?.setObject(image, forKey: key as NSString)
+    }
+    
+    private func getImageFromCache(from key: String) -> UIImage? {
+        return imageCache?.object(forKey: key as NSString) as? UIImage
     }
 }
