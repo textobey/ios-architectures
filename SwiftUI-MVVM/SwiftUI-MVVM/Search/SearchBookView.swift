40ddf512d7f40ecdcda7cc3994c55f3d478ace41
textobey <dogsan_e@naver.com>
2024-01-05 16:54:39 +0900

    [MOD] 검색창UI SwiftUI API로 변경

--- SwiftUI-MVVM/SwiftUI-MVVM/Search/SearchBookView.swift
+++ SwiftUI-MVVM/SwiftUI-MVVM/Search/SearchBookView.swift
@@ -11,8 +11,7 @@ import Combine
 struct SearchBookView: View {
     @ObservedObject var viewModel: SearchBookViewModel
     
-    @State var text: String = ""
-    @State var scopeSelection: Int = 0
+    @State var searchText: String = ""
     
     public init(viewModel: SearchBookViewModel) {
         self.viewModel = viewModel
@@ -29,31 +28,30 @@ struct SearchBookView: View {
                 }
             }
             .navigationTitle("Search Book")
-            .navigationSearchBar(
-                text: $text,
-                scopeSelection: $scopeSelection,
-                options: [
-                    .automaticallyShowsSearchBar: true,
-                    .obscuresBackgroundDuringPresentation: false,
-                    .hidesNavigationBarDuringPresentation: true,
-                    .hidesSearchBarWhenScrolling: false,
-                    .placeholder: "Search",
-                ],
-                actions: [
-                    .onCancelButtonClicked: {
-                        print("Cancel")
-                    },
-                    .onSearchButtonClicked: {
-                        print("Search")
-                    }
-                ]
+            .searchable(
+                text: $searchText,
+                // navigationBarDrawer: 검색창이 숨겨져있고, 네비게이션을 밑으로 내렸을때만 검색창이 표출
+                // navigationBarDrawer(displayMode: .always): 검색창이 숨겨져 있지 않고, 항상 표출
+                // automatic: iOS, iPad OS 각 플랫폼 마다 다르게 적용(iOS는 navigationBarDrawer)
+                // sideBar: sidebar에서 적용되는 검색창
+                // toolbar: toolbar에서 적용되는 검색창
+                placement: .navigationBarDrawer(displayMode: .always),
+                prompt: "Search"
             )
+            .onSubmit(of: .search) {
+                viewModel.transform(.search(searchText))
+            }
+            .onChange(of: searchText) { newValue in
+                viewModel.transform(.search(newValue))
+            }
         }
         // 트러블슈팅: StackNavigationViewStyle modifier를 추가하지 않으면 제약조건 오류 발생
         .navigationViewStyle(.stack)
         .overlay {
-            if viewModel.bookItems.isEmpty {
-                ContentEmptyView(message: "No Articles for \(text)")
+            if viewModel.bookItems.isEmpty && searchText.isEmpty {
+                ContentEmptyView(message: "No Results")
+            } else if viewModel.bookItems.isEmpty {
+                ContentEmptyView(message: "No Articles for '\(searchText)' ")
             }
         }
     }
