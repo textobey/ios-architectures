e36d8b0f195ce68e61a2b9e47e49ae8556613ce8
textobey <dogsan_e@naver.com>
2024-01-29 19:28:36 +0900

    [ADD] Testing in iOS: From Zero to Hero

---
+++ MyOceanTests/MyOceanTests/MyOceanTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOceanTestsApp.swift
+//  MyOceanTests
+//
+//  Created by 이서준 on 1/29/24.
+//
+
+import SwiftUI
+
+@main
+struct MyOceanTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
